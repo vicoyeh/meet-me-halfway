8b8280f139f41c78a8ee6bf22d07cfb8a7ebfbf6
Kevin Frans <kevin@bobthechicken.com>
2014-09-06 14:57:26 -0700

    init

---
+++ MeetUp/MeetUp/FirstViewController.m
@@ -0,0 +1,45 @@
+//
+//  FirstViewController.m
+//  MeetUp
+//
+//  Created by Kevin Frans on 9/6/14.
+//  Copyright (c) 2014 Kevin Frans. All rights reserved.
+//
+
+#import "FirstViewController.h"
+#import <MapKit/MapKit.h>
+
+#define METERS_PER_MILE 1609.344
+
+@interface FirstViewController ()
+@property (weak, nonatomic) IBOutlet MKMapView *map;
+
+@end
+
+@implementation FirstViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (void)viewWillAppear:(BOOL)animated {
+    // 1
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = 39.281516;
+    zoomLocation.longitude= -76.580806;
+    
+    // 2
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+    
+    // 3
+    [_mapView setRegion:viewRegion animated:YES];
+}
+
+@end
