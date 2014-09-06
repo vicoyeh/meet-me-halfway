fb7ec7aeb27579d4704f0d3044f3cd4401df73b0
wilzh40 <wilzh40@gmail.com>
2014-09-06 15:34:05 -0700

    Added singleton again

---
+++ ios App/MeetUp2/MeetUp2/Singleton.h
@@ -0,0 +1,13 @@
+//
+//  Singleton.h
+//  MeetUp2
+//
+//  Created by Wilson Zhao on 9/6/14.
+//  Copyright (c) 2014 Kevin Frans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Singleton : NSObject
+
+@end
---
+++ ios App/MeetUp2/MeetUp2/Singleton.m
@@ -0,0 +1,24 @@
+//
+//  Singleton.m
+//  MeetUp2
+//
+//  Created by Wilson Zhao on 9/6/14.
+//  Copyright (c) 2014 Kevin Frans. All rights reserved.
+//
+
+#import "Singleton.h"
+
+@implementation Singleton
++ (Singleton *)sharedInstance
+{
+    static Singleton *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[Singleton alloc] init];
+        // Do any other initialisation stuff here
+    });
+    return sharedInstance;
+}
+
+
+@end
