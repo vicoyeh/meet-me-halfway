28892fbe5c18922e66f6051b87cc86d30300502e
Ethan Yu <ethan@whatsapp.com>
2014-09-06 16:23:50 -0700

    Changed Storyboard, created groups

---
+++ ios App/MeetUp2/MeetUp2/MUFriendsListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MUFriendsListTableViewController.h
+//  MeetUp2
+//
+//  Created by Ethan Yu on 9/6/14.
+//  Copyright (c) 2014 Kevin Frans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUFriendsListTableViewController : UITableViewController
+
+@end
