d97dc502aabb6896d1c0d450831599a8cfbeebed
Kevin Frans <kevin@bobthechicken.com>
2014-09-06 16:56:35 -0700

    map on the new file

--- iOS/MeetUp2/MeetUp2/MappingViewController.m
+++ iOS/MeetUp2/MeetUp2/MappingViewController.m
@@ -7,31 +7,82 @@
 //
 
 #import "MappingViewController.h"
+#import <MapKit/MapKit.h>
 
 @interface MappingViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *map;
 
 @end
 
 @implementation MappingViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+{
+    CLLocationManager *locationManager;
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (float)deviceLocationLat {
+    return _map.userLocation.location.coordinate.latitude;
 }
-*/
+
+- (float)deviceLocationLong {
+    return _map.userLocation.location.coordinate.longitude;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.distanceFilter = kCLDistanceFilterNone; // whenever we move
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest; // 100 m
+    [locationManager startUpdatingLocation];
+    
+    [self performSelector:@selector(textFieldDidChange) withObject:nil afterDelay:2.0];
+    
+    
+}
+
+
+-(void) textFieldDidChange
+{
+    [self changeMap];
+    
+    [self performSelector:@selector(textFieldDidChange) withObject:nil afterDelay:2.0];
+}
+
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    [self changeMap];
+}
+
+-(void) changeMap
+{
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = [self deviceLocationLat];
+    zoomLocation.longitude= [self deviceLocationLong];
+    
+    NSLog(@"%d",[self deviceLocationLat]);
+    NSLog(@"%d",[self deviceLocationLong]);
+    
+    // 2
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+    
+    // 3
+    [_map setRegion:viewRegion animated:YES];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    // 1
+    [self changeMap];
+}
+
 
 @end
