8762db87d5888ae93d7dcfb63201065be9567b4a
Vic <vic317yeh@ucla.edu>
2014-09-07 05:37:13 -0700

    udpate

--- routes/account.js
+++ routes/account.js
@@ -22,9 +22,22 @@ router.post('/',[bodyParser.urlencoded(), bodyParser.json()], function(req, res)
   // var appleid = data.appleid;
   // var username = data.username;
 
+    var options = { };
 
+    var apnConnection = new apn.Connection(options);
+var myDevice = new apn.Device("<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>");
 
-  pushNotification.pushTo("<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>","John");
+var note = new apn.Notification();
+
+note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+note.badge = 3;
+note.sound = "ping.aiff";
+note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+note.payload = {'messageFrom': 'Caroline'};
+
+apnConnection.pushNotification(note, myDevice);
+
+  //pushNotification.pushTo("<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>","John");
 });
 
 module.exports = router;
