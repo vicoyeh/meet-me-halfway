067f8be25bcef234ad891f3f0f65dc6199e38d18
Vic <vic317yeh@ucla.edu>
2014-09-07 05:40:13 -0700

    udpate

--- routes/account.js
+++ routes/account.js
@@ -25,7 +25,7 @@ router.post('/',[bodyParser.urlencoded(), bodyParser.json()], function(req, res)
     var options = { };
 console.log("hi2");
     var apnConnection = new apn.Connection(options);
-var myDevice = new apn.Device("<30829bc234cfc1ece6f6ad3370dee3b6b00e8b2ca41ca1caa32cb10bad7ba6dc>");
+var myDevice = new apn.Device("<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>");
 console.log("hi3");
 var note = new apn.Notification();
 console.log("hi4");
