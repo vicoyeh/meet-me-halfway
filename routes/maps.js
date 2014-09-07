5f9f81a1e852dc0362319b0b86ca0551744bd84d
Vic <vic317yeh@ucla.edu>
2014-09-06 23:06:20 -0700

    update

--- routes/maps.js
+++ routes/maps.js
@@ -9,7 +9,7 @@ router.post('/', function(req, res) {
 	res.set('Content-Type','application/json');
 	console.log("Received req")
 	// console.log(req.body.toString());
-	var data = JSON.stringify(req.body.toString().slice(4,req.body.toString()-7));
+	var data = JSON.stringify(req.body).slice(4,req.body.toString()-7);
 	console.log(data);
 
   	var user = req.body.user;
