dae56446bf37d8dcc86bb2bf8c53e0de07bcd08f
Vic <vic317yeh@ucla.edu>
2014-09-07 06:31:33 -0700

    udpate

--- routes/account.js
+++ routes/account.js
@@ -46,11 +46,12 @@ apnConnection.on('transmissionError', function(err, nfn, dev){
         console.log("hi3");
         var note = new apn.Notification();
         console.log("hi4");
-        note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-        note.badge = 3;
-        note.sound = "ping.aiff";
-        note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
-        note.payload = {'messageFrom': 'Caroline'};
+        // note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+        // note.badge = 3;
+        // note.sound = "ping.aiff";
+        // note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+        // note.payload = {'messageFrom': 'Caroline'};
+        note.setAlertText("Hello, from node-apn!");
         console.log("hi5");
 
         apnConnection.pushNotification(note, ["<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>"]);
