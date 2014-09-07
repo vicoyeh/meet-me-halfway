b7d487075a25971afefb5578ee630606160142fc
Vic <vic317yeh@ucla.edu>
2014-09-07 04:49:44 -0700

    update

--- routes/account.js
+++ routes/account.js
@@ -25,7 +25,7 @@ router.post('/',[bodyParser.urlencoded(), bodyParser.json()], function(req, res)
 
 
 
-  pushNotification.pushTo("<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>");
+  pushNotification.pushTo("<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>","John");
 });
 
 module.exports = router;
