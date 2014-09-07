d6cca66233217cb3fa6956593f11ad902df0ab1d
Vic <vic317yeh@ucla.edu>
2014-09-07 04:50:10 -0700

    update

---
+++ models/push.js
@@ -0,0 +1,29 @@
+var apn = require('apn');
+
+//setup
+var options = { };
+var apnConnection = new apn.Connection(options);
+
+
+
+var model = function() {
+
+	return  {
+
+		pushTo : function(id,name) {
+			var myDevice = new apn.Device(id);
+
+			var note = new apn.Notification();
+			note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+			note.badge = 3;
+			note.sound = "ping.aiff";
+			note.alert = "\uD83D\uDCE7 \u2709 You have a new invitation";
+			note.payload = {'messageFrom': "Tom"};
+
+			apnConnection.pushNotification(note, myDevice);
+		}
+	}
+
+}
+
+module.exports = model;
