dc85cb3fd898bd6ffb91ee6d669f85e60cadd7a3
Vic <vic317yeh@ucla.edu>
2014-09-06 23:18:33 -0700

    update

--- routes/maps.js
+++ routes/maps.js
@@ -6,7 +6,7 @@ var map_model = require('../models/maps')();
 
 
 
-router.post('/', bodyParser.json(), function(req, res) {
+router.post('/',[express.urlencoded(), express.json()], function(req, res) {
 	res.set('Content-Type','application/json');
 	console.log("Received req")
 	// console.log(req.body.toString());
