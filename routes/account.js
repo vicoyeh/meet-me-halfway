243fe09e50718c9a12daa03d5fda2eec12745aae
Vic <vic317yeh@ucla.edu>
2014-09-07 05:52:56 -0700

    update

--- routes/account.js
+++ routes/account.js
@@ -24,7 +24,7 @@ router.post('/',[bodyParser.urlencoded(), bodyParser.json()], function(req, res)
 
     var options = {cert:__dirname + '/cert.pem',key:__dirname+'/key.pem',gateway:'gateway.sandbox.push.apple.com' };
 console.log("hi2");
-    var apnConnection = new apn.connection(options);
+    var apnConnection = new apn.Connection(options);
     console.log("hi2.5");
 var myDevice = new apn.Device("<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>");
 console.log("hi3");
