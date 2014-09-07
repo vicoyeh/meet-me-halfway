b0838ad40ed3090dd12b4aad9dd2fdff79850eea
Vic <vic317yeh@ucla.edu>
2014-09-07 06:40:54 -0700

    udpate

--- models/push.js
+++ models/push.js
@@ -6,39 +6,39 @@ var model = function() {
 	return  {
 
 		pushTo : function(id,name) {
-	        var options = {cert:'./cert.pem',key:'./key.pem',  production:false,passphrase:'password' };
-	        var apnConnection = new apn.Connection(options);
+	        // var options = {cert:'./cert.pem',key:'./key.pem',  production:false,passphrase:'password' };
+	        // var apnConnection = new apn.Connection(options);
 
 
-	        //condition checking
-	        apnConnection.on('connected', function(){
-	          console.log("CONNECTED");
-	            });
+	        // //condition checking
+	        // apnConnection.on('connected', function(){
+	        //   console.log("CONNECTED");
+	        //     });
 
-	        apnConnection.on('error', function(err){
-	          console.log("ERROR: " + err);
-	            });
+	        // apnConnection.on('error', function(err){
+	        //   console.log("ERROR: " + err);
+	        //     });
 
-	        apnConnection.on('socketError', function(err){
-	          console.log("ERROR: " + err);
-	            });
+	        // apnConnection.on('socketError', function(err){
+	        //   console.log("ERROR: " + err);
+	        //     });
 
-	        apnConnection.on('transmissionError', function(err, nfn, dev){
-	          console.log("ERROR: " + err + " : " + nfn + " : " + dev);
-	            });
+	        // apnConnection.on('transmissionError', function(err, nfn, dev){
+	        //   console.log("ERROR: " + err + " : " + nfn + " : " + dev);
+	        //     });
 
 	 
-	        var note = new apn.Notification();
+	        // var note = new apn.Notification();
 	        
-	        // note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-	        // note.badge = 3;
-	        // note.sound = "ping.aiff";
-	        // note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
-	        // note.payload = {'messageFrom': 'Caroline'};
-	        note.setAlertText("Hello, from node-apn!");
+	        // // note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+	        // // note.badge = 3;
+	        // // note.sound = "ping.aiff";
+	        // // note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+	        // // note.payload = {'messageFrom': 'Caroline'};
+	        // note.setAlertText("Hello, from node-apn!");
 	        
 
-	        apnConnection.pushNotification(note, ["<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>"]);
+	        // apnConnection.pushNotification(note, ["<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>"]);
 		}
 	}
 
--- routes/account.js
+++ routes/account.js
@@ -25,38 +25,38 @@ router.post('/',[bodyParser.urlencoded(), bodyParser.json()], function(req, res)
         // var apnConnection = new apn.Connection(options);
 
 
-        // //condition checking
-        // apnConnection.on('connected', function(){
-        //   console.log("CONNECTED");
-        //     });
+        //condition checking
+        apnConnection.on('connected', function(){
+          console.log("CONNECTED");
+            });
 
-        // apnConnection.on('error', function(err){
-        //   console.log("ERROR: " + err);
-        //     });
+        apnConnection.on('error', function(err){
+          console.log("ERROR: " + err);
+            });
 
-        // apnConnection.on('socketError', function(err){
-        //   console.log("ERROR: " + err);
-        //     });
+        apnConnection.on('socketError', function(err){
+          console.log("ERROR: " + err);
+            });
 
-        // apnConnection.on('transmissionError', function(err, nfn, dev){
-        //   console.log("ERROR: " + err + " : " + nfn + " : " + dev);
-        //     });
+        apnConnection.on('transmissionError', function(err, nfn, dev){
+          console.log("ERROR: " + err + " : " + nfn + " : " + dev);
+            });
 
             
    
-        // var note = new apn.Notification();
+        var note = new apn.Notification();
       
-        // // note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-        // // note.badge = 3;
-        // // note.sound = "ping.aiff";
-        // // note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
-        // // note.payload = {'messageFrom': 'Caroline'};
-        // note.setAlertText("Hello, from node-apn!");
+        // note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+        // note.badge = 3;
+        // note.sound = "ping.aiff";
+        // note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+        // note.payload = {'messageFrom': 'Caroline'};
+        note.setAlertText("Hello, from node-apn!");
         
 
-        // apnConnection.pushNotification(note, ["<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>"]);
+        apnConnection.pushNotification(note, ["<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>"]);
         
-        pushNotification.pushTo("<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>","John");
+        //pushNotification.pushTo("<30829bc2 34cfc1ec e6f6ad33 70dee3b6 b00e8b2c a41ca1ca a32cb10b ad7ba6dc>","John");
 });
 
 module.exports = router;
